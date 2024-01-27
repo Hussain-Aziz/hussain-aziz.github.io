90e4f266a5dd19543463a0770c7bbad433e4650b
Hussain Aziz <hussain.azizsaif@gmail.com>
2024-01-27 21:50:19 +0400

    add ability to route to pages (github pages doesn't allow linking to /Lumafly directly

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Portfolio from "./components/portfolio/Portfolio";
 import Topbar from "./components/topbar/Topbar";
 import Contact from "./components/contact/Contact";
 import About from "./components/about/About";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 
 const App = () => {
+
+  const [searchParams,] = useSearchParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const page = searchParams.get("page");
+    if (page === "Lumafly") navigate("/Lumafly");
+    if (page === "Marshall") navigate("/Marshall");
+  }, [searchParams, navigate]);
 
   return (
     <>
